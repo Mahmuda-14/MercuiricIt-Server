0a07e81dc89e25a297e8fce09ee727f84534686a
Sadia0011 <sadiamoon2019@gmail.com>
2024-04-30 01:47:14 +0600

    added pagination routes

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ app.use(cors());
 
 // /mongodb
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uok4zlq.mongodb.net/?retryWrites=true&w=majority`;
-const uri = "mongodb+srv://merquireit:bLLOxvuYH70NVNg4@cluster0.uok4zlq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uok4zlq.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -38,6 +37,47 @@ async function run() {
     //   await client.connect();
       // Send a ping to confirm a successful connection
     //   await client.db("admin").command({ ping: 1 });
+
+    const profileCollection = client.db('profileDB').collection('profile');
+
+
+    app.get('/pro', async (req, res) => {
+      const cursor = profileCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+
+    })
+
+
+
+    // sending data to db
+    app.post('/profile', async (req, res) => {
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await profileCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+
+    // pagination
+app.get('/itemsCount', async (req, res) => {
+    const count = await profileCollection.countDocuments();
+    res.send({ count });
+  })
+  
+  app.get('/items', async (req, res) => {
+    const page = parseInt(req.query.page);
+    const size = parseInt(req.query.size);
+  
+    // console.log('pagination query', page, size);
+    const result = await profileCollection.find({ })
+    .skip(page * size)
+    .limit(size)
+    .toArray();
+    // console.log(result)
+    res.send(result);
+  })
+
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
       // Ensures that the client will close when you finish/error
